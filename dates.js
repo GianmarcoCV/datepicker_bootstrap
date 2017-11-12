804e549d016fccf81a74455ba3b7bb325994cc9a
GianmarcoCV <gianmarcotv@hotmail.com>
2017-11-12 01:04:31 -0500

    Create dates.js
    	//PERMITE CONTROLAR LA FECHA INICIO Y FIN SEGUN LOS DESPLEGABLES.

---
+++ dates.js
@@ -0,0 +1,17 @@
+
+/* 	$("#fechainicio").datepicker({
+ 		endDate: "0d",
+ 		todayHighlight: true  
+ 	}).on('changeDate', function (selected) {
+ 		var minDate = new Date(selected.date.valueOf());
+ 		$('#fechafin').datepicker('setStartDate', minDate);
+ 	});
+
+ 	$("#fechafin").datepicker({
+ 		endDate: "0d",
+ 		todayHighlight: true  
+ 	})
+ 	.on('changeDate', function (selected) {
+ 		var minDate = new Date(selected.date.valueOf());//opcional si quieres que fecha1 fin sea esta
+ 		$('#fechainicio').datepicker('setEndDate', minDate);
+ 	});*/
